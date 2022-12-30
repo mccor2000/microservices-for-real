25f65b47da510d01aefcf70ebc157a3232bced2a
mccor <roccm0200@gmail.com>
2022-12-30 21:45:13 +0700

    chore: remove hardcoded partition

--- order-lifecycle/src/infrastructure/order.event-publisher.ts
+++ order-lifecycle/src/infrastructure/order.event-publisher.ts
@@ -1,23 +1,24 @@
-import { KafkaProducerService } from "@microservices-for-real/common";
-import { Inject, Injectable, Logger } from "@nestjs/common";
-import { IOrderEventPublisher } from "src/domain/order.ports";
+import { KafkaProducerService } from '@microservices-for-real/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { IOrderEventPublisher } from 'src/domain/order.ports';
 
 @Injectable()
 export class OrderEventPublisher implements IOrderEventPublisher {
-    protected logger = new Logger(OrderEventPublisher.name)
+  protected logger = new Logger(OrderEventPublisher.name);
 
-    constructor(
-        @Inject('KAFKA_PRODUCER_SERVICE')
-        private readonly producerService: KafkaProducerService
-    ) { }
+  constructor(
+    @Inject('KAFKA_PRODUCER_SERVICE')
+    private readonly producerService: KafkaProducerService,
+  ) {}
 
-    async produce(eventTopic: string, eventKey: string, data: any): Promise<void> {
-        return this.producerService.produce(eventTopic,
-            {
-                key: eventKey,
-                value: JSON.stringify(data),
-                partition: 1,
-            }
-        )
-    }
+  async produce(
+    eventTopic: string,
+    eventKey: string,
+    data: any,
+  ): Promise<void> {
+    return this.producerService.produce(eventTopic, {
+      key: eventKey,
+      value: JSON.stringify(data),
+    });
+  }
 }
